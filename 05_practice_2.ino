5bfbd6affbe9fbf6d0569d96728223fd1c467acc
Lee sangjin-kmu <84307876+Sangjin-km@users.noreply.github.com>
2021-09-15 15:02:30 +0900

    Update 05_practice_2.ino

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -8,7 +8,7 @@ void loop() {
   delay(1000);
   digitalWrite(PIN7, LOW);
   delay(1000);
-  while(a<6) {
+  while(a<6) { // a<6이면 0~5까지 6번 반복인데 LED는 5번만 켜짐 반대로 a<5하면 4번만 켜짐
     digitalWrite(PIN7, HIGH);
     delay(100);
     digitalWrite(PIN7, LOW);
